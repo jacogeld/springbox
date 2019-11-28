0361786bec9d3779294f4fd21f1ae88a4817c71c
Caleb Zeeman <caleb.zeeman@gmail.com>
2019-11-28 16:57:53 +0200

    Version1. MPI working and results as expected. Documentation complete

--- dist/src/search.c
+++ dist/src/search.c
@@ -1,10 +1,13 @@
 /******************************************************************************
- *	TODO: Describe code
+ *	Finds the longest string that has no repeated boxes. Utilises MPI to run
+ *	multiple workers at once. Relies on a queue-based system to handle jobs
+ *	between workers.
+ *
  *	compile:	mpicc -o ../bin/search.o queue.c search.c
  *	run:		mpirun -np X ../search.o		where X is number processes
  *
  *	@author C. R. Zeeman (caleb.zeeman@gmail.com)
- *	@version 0.1
+ *	@version 1.0
  *	@date 2019-11-28
  *****************************************************************************/
 /* Includes */
@@ -15,14 +18,15 @@
 #include "queue.h"
 
 /************************** Definitions and macros **************************/
+/* Configurations */
+#define DEBUG (0)						/* Enables debug output */
+#define STANDARD_SEARCH_DEPTH (5)		/* Depth to search per job */
+#define INITIAL_SEARCH_DEPTH (3)		/* Depth for initial run by ROOT */
+#define STANDARD_ALPHABET_SIZE (3)		/* Alphabet size */
 
-#define DEBUG (0)
-#define STANDARD_SEARCH_DEPTH (4)
-#define INITIAL_SEARCH_DEPTH (3)
-#define STANDARD_ALPHABET_SIZE (3)
-
+/* Do not modify */
 #define LAST ('/' + STANDARD_ALPHABET_SIZE)
-#define VALID (1)
+#define VALID (1)						/* Signals for valid and invalid */
 #define INVALID (0)
 
 /* Constants for MPI signalling */
@@ -36,17 +40,13 @@
 #define EXPECT_QUEUE_TAG	(2006)
 #define QUEUE_TAG			(2007)
 
+/* TODO: Allow dynamic setting via cmd line */
+
 /*************************** global variables ******************************/
 
 /* Alphabet used -> alpha_size subset of alpha[] */
-/* TODO: Allow dynamic setting via cmd line */
 
-/* TODO: Documentation */
-/* TODO: Changelog. Leave todos in after working V1, then can do versions
- * afterwards (e.g. 1.1, 1.2) */
-
- /* BUG: Occasionally an invalid case gets through process, even though the
-  * check tools pick it up as invalid if manually checking */
+/* BUG: occasionally an invalid case ends up on the queue */
 int alpha_size = STANDARD_ALPHABET_SIZE;
 char alpha[] = {'0','1','2','3','4','5', '6', '7', '8', '9'};
 
@@ -58,21 +58,31 @@ char *max_word;
 int my_id;		/* Process ID; set in main */
 /*********************** function prototypes *******************************/
 
-/* TODO: rename box_ok to box_repeat_exists */
-int box_repeat_exists(char *word, int len);	/* TODO: Convert to inline*/
 void process (char *word, int depth);		/* TODO: convert to inline*/
-int box_ok(char *word, int len); 	/* TODO: remove and use 1 */
-int box_ok_1(char *word, int len);
-void process_1(char *word, int depth);	/* "" */
-
-void manual_check(char *string, int len);	/*Check if pattern is box-valid */
+int box_ok(char *word, int len); 			/* TODO: refactor and use below */
+int box_valid(char *word, int len);			
+int deep_check(char *string, int len);		
+void manual_check(char *string, int len);
+void process_1(char *word, int depth);
 
 /**************************** functions ***********************************/
 
-/* TODO: Make all cases of 'enqueue' send the signal and data to root process if
- * it is not the root process so it can be enqueued. Or merge this into the
- * actual enqueue function (so it recieves in that function) */
-
+/* ROOT_PROCESS handles the queue, while all other processes do the
+ * calculations. Communication is done via tags to denote message types.
+ * Communcation cycle as follows:
+ * ROOT does one iteration and adds items to queue, then sends one job to each
+ * workers. The workers process the job, and once reach specified depth, sends a
+ * signal back to the ROOT to recieve the item to be queued, followed by the
+ * string itself. Once complete, the worker sends the maximum size it found back
+ * to ROOT. if this is longer than previous maximum found by all processes, ROOT
+ * requests the string and stores it. It then sends the next workload to the
+ * process, and the cycle repeats until a) the queue is empty, or b) a string of
+ * theoretical maximum length is found. A stop signal is then sent to all
+ * workers (at appropriate times), and once all workers have stopped, ROOT
+ * reports the maximum and stops itself.
+ *
+ * @args none
+*/
 int main(int argc, char *argv[])
 {
 	/* Setup */
@@ -105,13 +115,6 @@ int main(int argc, char *argv[])
 
 	/* Root/Master */
 	if (my_id == ROOT_PROCESS) {
-		/*DELME*/
-		/*
-		char str[] = "012001210122010201120210212012";
-		manual_check(str, sizeof(str)/sizeof(char));
-		char str2[] = "0220102012101210012011021202102";
-		manual_check(str2, sizeof(str2)/sizeof(char));
-		*/
 		/* Initial run to create branches for workers */
 		w[0] = '0';
 		w[1] = '\0';
@@ -119,7 +122,7 @@ int main(int argc, char *argv[])
 
 		MPI_Barrier(MPI_COMM_WORLD);
 
-		/* TODO: Send initial instructions to each process */
+		/* Send initial instructions to each process */
 		for (an_id = 1; an_id < nbr_procs; an_id++) {
 			if (!dequeue(w)) {
 				break;	/* Failsafe */	
@@ -164,15 +167,15 @@ int main(int argc, char *argv[])
 				}	
 				/* Send stop command if needed... */
 				if (stop) {
-					/* TODO: Keep track of which processes stopped and if all
-					 * stopped, then main can also end */
+					/* Keep track of number of processes still running.
+					 * If all stopped, then ROOT_PROCESS can also end */
 					 MPI_Send(&send_count, 1, MPI_INT, an_id, STOP_TAG,
 					 	MPI_COMM_WORLD);
 					active_processes--;
 					if (active_processes == 0) {
 						break;
 					}
-				} /* or send the next work available in queue */
+				} /* ...or send the next work available in queue */
 				else if (!dequeue(w)) {
 					break;	/* Failsafe */
 				}
@@ -198,14 +201,6 @@ int main(int argc, char *argv[])
 				}
 				enqueue(w);
 			}
-			/*
-			...
-			update max size by getting result from workers */
-			/* TODO: two way communication: process sends max length back.
-			 * Then main sends command signal: 1 int stating either "sending
-			 * next workload", or "send me the max string back", or "stop" 
-			 */
-			/*...*/
 			queue_size = get_queue_size();
 		} 
 		/* qs == 0 => explored all || ml == ds => found a longest pattern */
@@ -215,13 +210,12 @@ int main(int argc, char *argv[])
 		printf("max == %d\n", max_length);
 		printf("max word == %s\n", max_word);
 		printf("queue size == %d\n", get_queue_size());
+		printf("max valid? %d\n", deep_check(max_word, max_length));
 	}
 	
 	/* Worker/Slave */
 	else {
 		MPI_Barrier(MPI_COMM_WORLD);
-
-		/* TODO: exception if count higher than array size */
 
 		/* Continue until a message is recieved stating you must stop */
 		while (status.MPI_TAG != STOP_TAG) {
@@ -237,7 +231,9 @@ int main(int argc, char *argv[])
 
 			 /* else if command is to get new work, get new work string */
 			else if (status.MPI_TAG == NEW_WORK_TAG) {
-				if (DEBUG) printf("*:rec_count == %d\n", rec_count);
+				if (DEBUG) {
+					printf("*:rec_count == %d\n", rec_count);
+				}
 				ierr = MPI_Recv(w, rec_count, MPI_CHAR, ROOT_PROCESS,
 					WORK_TAG, MPI_COMM_WORLD, &status);
 
@@ -259,23 +255,14 @@ int main(int argc, char *argv[])
 	printf("***** PROCESS %d STOPPED ******\n", my_id);
 	return EXIT_SUCCESS;
 }
-
-void process_1(char *word, int depth) {
-	int k = strlen(word), i = k, valid = 1, char_sel = 0;
-
-	while (i >= k) {
-		break;
-		/* Insert */
-		/* Check if valid */
-		/* if valid, increment i */
-			/* if i = k+depth, queue and i-- */
-		/* increment w[i] */
-		/* if w[i] > '0' +  alpha_size - 1*/
-			/* i-- */
-	}
-}
-
-/* Jaco's process code. TODO: Refactor */
+/*  Jaco's process code. TODO: Refactor 
+ *	Adds characters to pattern until certain depth is reached, checking that
+ *	each new addition is box-valid. Once certain depth is reached, the remaining
+ *	possible branches are added to the queue for future iterations to handle.
+ *
+ * @param word	the string/pattern so far
+ * @param depth	how far to travel along the branches before queueing
+*/
 int limit = 10000;
 
 void process(char *word, int depth) {
@@ -288,6 +275,8 @@ void process(char *word, int depth) {
 		printf("i==%d\n", i);
 		if (limit-- < 0) { exit(1); }
 	}
+	/* TODO: remove this once queueing issue is gone */
+	if (deep_check(word,i) == INVALID) return;
 	word[i] = '/';
 	while (i >= k) {
 		if (DEBUG) {
@@ -326,7 +315,7 @@ void process(char *word, int depth) {
 			i--;
 			continue;
 		}
-		valid = box_ok_1(word,i);
+		valid = box_valid(word,i);
 		if (valid) {
 			if (DEBUG) {
 				printf("no repeat box\n");
@@ -342,10 +331,12 @@ void process(char *word, int depth) {
 			printf("repeated box\n");
 		}
 	}
-	if (DEBUG) printf("Done process\n");
+	if (DEBUG) {
+		printf("Done process\n");
+	}
 }
 
-/* Jaco's Box_ok*/
+/* Jaco's Box_ok function */
 int box_ok(char* w, int i) {
 	int j = i - 1;
 	while ((j >= 0) && (i - j <= STANDARD_ALPHABET_SIZE) && (w[i] != w[j])) {
@@ -368,16 +359,22 @@ int box_ok(char* w, int i) {
 	return 1;
 }
 
+/* checks if the last character added to a pattern is box-valid (i.e. the box
+ * ending in the len'th character is never repeated.
+ *
+ * @param pattern	pattern/string to be checked
+ * @param len		length of pattern
+ * @return		VALID or INVALID respectively
+ */
 
-/* TODO: Rename & Refactor */
-int box_ok_1(char* pattern, int len)
-{
+/* TODO: Refactor */
+int box_valid(char* pattern, int len) {
 	char c, c_end;
 	int count = 1, b1_end = len, b1_start = b1_end, b2_start, b2_end, i = 0;
 	int lb1, lb2;
 	/* Step back until find char at pattern[len-1]
 	 * Remember this box. Find 3rd repeat, check box. find 4th repeat. Check 3-4
-	 * box and compare to 1-2 box. Repeat all boxes 
+	 * box and compare to 1-2 box. Repeat this for all possible boxes 
 	 * optimisation: skip if boxes too long (> alpha_size) TODO
 	*/
 	/* Find box */
@@ -439,15 +436,91 @@ int box_ok_1(char* pattern, int len)
 	return VALID;
 }
 
+/*	Iterates over an entire pattern to confirm if it box-valid
+ *	
+ *	@param str	string to be checked
+ *	@param len	length of string
+ *	@return VALID or INVALID respectively
+ */
+int deep_check(char* str, int len) {
+	char *c = str;
+	char *w = malloc(sizeof(char) * (len+1));
+	int i, valid = 1;
+	for (i = 0; i < len; i++) {
+		w[i] = c[i];
+		w[i+1] = '\0';
+		valid = box_valid(w, i);
+		if (valid == 0) return INVALID;
+	}
+	return VALID;
+}
+
+/*	Iterates over the entire pattern, adding characters iterably and states
+ *	whether the pattern is box-valid at each character added.
+ *	(Like deepcheck, except reports via stdout at each iteration)
+ *
+ *	@param str 	string to be checked
+ *	@param len	length of string
+ */
 void manual_check(char *str, int len) {
 	char *c = str;
 	char *w = malloc(sizeof(char) * (len + 1));
-	int i;
+	int i, valid = 1;
 	printf("local test\n");
 	for (i = 0; i < len; i++) {
 		w[i] = c[i];
 		w[i+1] = '\0';
-		printf("i:%d\tValid:%d\n", i, box_ok(w, i));
+		valid = box_ok(w,i);
+		printf("i:%d\tValid:%d\n", i, valid);
 	}
 	free(w);
 }
+
+/* Refactoring in progress. Do not use */
+void process_1(char *word, int depth) {
+	int k = strlen(word), i = k, valid = 1, diff = alpha[0] + alpha_size - 1;
+	char nul = alpha[0] - 1;
+
+	word[i] = nul;
+	while (i >= k) {
+		if (i == k + depth) {
+			word[i] = '\0';
+			if (my_id != ROOT_PROCESS) {
+			/* TODO: Move to new enqueue_d function*/
+				MPI_Send(&i, 1, MPI_INT, ROOT_PROCESS, EXPECT_QUEUE_TAG,
+					MPI_COMM_WORLD);
+				MPI_Send(word, i, MPI_CHAR, ROOT_PROCESS, QUEUE_TAG,
+					MPI_COMM_WORLD);
+			} else {
+				if (DEBUG) printf("(1)about to enqueue %s\n", word);
+				enqueue(word);
+			}
+			i--;
+			continue;
+		}
+		word[i]++;
+			/* outside bounds*/
+		if (word[i] >  diff) {
+			i--;
+			continue;
+		}
+		if (box_ok(word, i)) {
+			i++;
+			if (i > max_length) {
+				max_length = i;
+				word[i] = '\0';
+				strcpy(max_word, word);
+			}
+			word[i] = nul;
+		}
+		/* Insert */
+		/* Check if valid */
+		/* if valid, increment i */
+			/* if i = k+depth, queue and i-- */
+		/* increment w[i] */
+		/* if w[i] > '0' +  alpha_size - 1*/
+			/* i-- */
+	}
+}
+
+
